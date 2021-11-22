36f2db3369abe5dedfcd608ad956dbc7a9101578
SamSepiool <84787501+SamSepiool@users.noreply.github.com>
2021-11-22 16:00:51 +0100

    add select for categories

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -8,6 +8,8 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
 use App\Post;
+use App\Category;
+
 
 
 class PostController extends Controller
@@ -31,7 +33,8 @@ class PostController extends Controller
      */
     public function create()
     {
-        return view('admin.posts.create');
+        $categories = Category::all();
+        return view('admin.posts.create', compact('categories'));
     }
 
     /**
@@ -42,7 +45,6 @@ class PostController extends Controller
      */
     public function store(Request $request)
     {
-
         $newPost = new Post;
 
         $newPost->fill($request->all());
--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -28,6 +28,16 @@
                         <textarea class="form-control" name="content" id="content" cols="30" rows="10" placeholder="Write the post's content">{{ old('content')}}</textarea>
                     </div>
 
+                    <div class="form-group">
+                        <label for="category">Category</label>
+                        <select name="category_id" class="form-control">
+                            <option value="">Select category</option>
+                            @foreach ($categories as $category)
+                                <option value="{{$category['id']}}">{{$category['name']}}</option>
+                            @endforeach
+                        </select>
+                    </div>
+
                     <button type="submit" class="btn btn-success">Add post</button>
                 </form>
                 </div>
