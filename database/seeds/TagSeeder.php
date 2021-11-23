829600e5274addc4269b01430735c5900188e4a8
SamSepiool <84787501+SamSepiool@users.noreply.github.com>
2021-11-23 16:30:15 +0100

    add tags posts relation

---
+++ database/seeds/TagSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Tag;
+use Illuminate\Support\Str;
+
+class TagSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $tags=['frontend','backend','ux','design patters'];
+        
+        foreach($tags as $tag){
+            $newTag= new Tag();
+            $newTag->name=$tag;
+            $newTag->slug=Str::of($newTag->name)->slug('-');
+            $newTag->save();
+        }
+    }
+}
